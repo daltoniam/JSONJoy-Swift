ed9961306400ec05b9309626ff46f1fd8dba1be3
Austin Cherry <chillingwithim@gmail.com>
2014-10-09 09:37:45 -0700

    added osx framework support

--- JSONJoy.h
+++ JSONJoy.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Vluxe. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for JSONJoy.
 FOUNDATION_EXPORT double JSONJoyVersionNumber;
