92d4096ae728628265ee9bfa22a1314b9b5a7743
Dalton Cherry <daltoniam@gmail.com>
2015-04-09 15:31:47 -0700

    swift 1.2

--- JSONJoy.swift
+++ JSONJoy.swift
@@ -111,7 +111,7 @@ public class JSONDecoder {
         } else if let dict = rawObject as? NSDictionary {
             var collect = Dictionary<String,JSONDecoder>()
             for (key,val: AnyObject) in dict {
-                collect[key as String] = JSONDecoder(val)
+                collect[key as! String] = JSONDecoder(val)
             }
             value = collect
         } else {
@@ -123,7 +123,7 @@ public class JSONDecoder {
         get {
             if let array = self.value as? NSArray {
                 if array.count > index {
-                    return array[index] as JSONDecoder
+                    return array[index] as! JSONDecoder
                 }
             }
             return JSONDecoder(createError("index: \(index) is greater than array or this is not an Array type."))
@@ -134,7 +134,7 @@ public class JSONDecoder {
         get {
             if let dict = self.value as? NSDictionary {
                 if let value: AnyObject = dict[key] {
-                    return value as JSONDecoder
+                    return value as! JSONDecoder
                 }
             }
             return JSONDecoder(createError("key: \(key) does not exist or this is not a Dictionary type"))
